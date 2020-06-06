e3ee3947c6e92899fac1c62b24c63af616353e39
fafnir-dev <dromanov260@gmail.com>
2020-06-06 22:04:51 +0500

    3 класса Перс, Герой, Солдат Класс Перс Инит списков Инит основного класса с енератором ид обязательным параметром тим метод вывода инфо по персу
    Класс Герой
    дополняет основной инит уровнем и именем героя
    метод повышения уровня
    
    Класс Солдат
    пуст

--- inheritanse.py
+++ inheritanse.py
@@ -1,11 +1,11 @@
 """
-Создать класс персонаж
-Свойсва Персонаж: ИД, номер команды
+Создать класс персонаж +
+Свойсва Персонаж: ИД, номер команды +
 
-Создать класс Герой наследник от класа Персонаж
-Свойства Героя: возможность повышать свой уровень
+Создать класс Герой наследник от класа Персонаж +
+Свойства Героя: возможность повышать свой уровень +
 
-Создать класс Солдат наследник от класса Персонаж
+Создать класс Солдат наследник от класса Персонаж +
 Свойства Солдата: следовать за героем своей команды
 
 В основной ветке программы создается по одному герою для каждой команды.
@@ -19,28 +19,54 @@
 Выведите на экран идентификационные номера этих двух юнитов.
 """
 
-import random #генератор рандома
+import random  # генератор рандома
+
 
 class Person:
-    def __init__(self, id, nteam):
-        self.id = id
-        self.nteam = nteam
+    # инит списков
+    army = []
+    good_army = []
+    bad_army = []
+
+    def __init__(self, team):  # инит основнового класса с рандомом ИД и команды
+        self.id = id(self)
+        self.team = team
+
+    def info_person(self):  # вывод описания перосонажа в зависимости от класса
+        if self.__class__.__name__ == 'Hero':
+            return self.id, self.team, self.name, self.level
+        else:
+            return self.id, self.team
+
 
 class Hero(Person):
-    def __init__(self, id, nteam, level=0):
-        Person.__init__(self, id, nteam)
+    def __init__(self, name, team, level=0):  # инит класса герой
+        Person.__init__(self, team)
+        self.name = name
         self.level = level
 
-    def level_up(self):
+    def level_up(self):  # инит левел апа
         self.level += 1
         return self.level
 
-class Solider(Person):
-    def go_to_hero(self):
-        print(f"I`m go to my Hero")
 
-Team1, Team2 = [], []
-Hero1 = Hero(1, random.randint(1, 2))
-Hero2 = Hero(2, random.randint(1, 2))
+class Solider(Person):  # Инит класс солдата
+    def follow(self):  # инит следовать за героем
+        pass
 
+
+# инит героев
+heroes = [
+    Hero("Gendalf", 'good'),
+    Hero("Sauron", 'bad')
+]
+
+for i in range(1, 10):
+    Person.army.append(Solider(random.choice(['good', 'bad'])))
+
+for i in Person.army:
+    if i.team == 'good':
+        Person.good_army.append(i)
+    else:
+        Person.bad_army.append(i)
 
