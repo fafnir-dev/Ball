ef11bb63ebda465d3fc28bf0023040f4f7daeca0
fafnir-dev <dromanov260@gmail.com>
2020-06-06 17:22:35 +0500

    create class add in list obj sorted ad print fired by qual

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import random
+
+
 class Person():
-    def __init__(self,name,surname):
+    def __init__(self, name, surname, qual=1):
         self.name = name
         self.surname = surname
+        self.qual = qual
 
-p = Person("Bob", "Marley")
-p1 = Person("Jack", "Daniels")
-print(p.name, p.surname)
-print(p1.name, p1.surname)
+    def print_person(self):
+        print(f"Name: {self.name} Surname: {self.surname} Quality: {self.qual}")
+
+    def fired_person(self):
+        print(f"Goodbay {self.name} {self.surname}")
+
+
+p = [
+     Person("Bob", "Marley", random.randint(1, 11)),
+     Person("Don", "Baron", random.randint(1, 11)),
+     Person("Donald", "Dack", random.randint(1, 11))
+    ]
+
+fired = sorted(p, key=lambda Person: Person.qual, reverse=True)
+
+for i in fired:
+    i.print_person()
+
+fired[-1].fired_person()
